85303c3359c38aca1271d339ec083d041c67800a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 18:49:07 -0400

    docs: Add required docstring to the conftest for dataanalysis tests.

--- dataanalysis/tests/conftest.py
+++ dataanalysis/tests/conftest.py
@@ -3,8 +3,9 @@
 import pytest
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def session_setup_teardown():
+    """Execute arbitrary operations before and after running a test suite."""
     print("Before test suite execution")
     yield
     print("After test suite execution")
