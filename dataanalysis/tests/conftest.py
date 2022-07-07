9da06758b4a5cbc80b8e23aac62e8ea9cf4cbf18
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 19:05:25 -0400

    feat: Update the conftest so that it uses fiasco to save JSON file.

--- dataanalysis/tests/conftest.py
+++ dataanalysis/tests/conftest.py
@@ -2,10 +2,13 @@
 
 import pytest
 
+from dataanalysis import fiasco
+
 
 @pytest.fixture(scope="session", autouse=True)
 def session_setup_teardown():
     """Execute arbitrary operations before and after running a test suite."""
     print("Before test suite execution")
     yield
+    fiasco.save_tracker()
     print("After test suite execution")
