100288d9fca1daee201f9a0b42864bbe3b21d118
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 18:52:41 -0400

    refactor: Use appropriate function and variable names in fiasco.

--- dataanalysis/dataanalysis/fiasco.py
+++ dataanalysis/dataanalysis/fiasco.py
@@ -10,14 +10,14 @@ LITTLE = "little"
 SINGLE_BLANK_SPACE = " "
 
 
-def convertToNumber(s):
+def convert_to_number(textual):
     """Convert from a textual representation to a numerical value."""
-    return int.from_bytes(s.encode(), "little")
+    return int.from_bytes(textual.encode(), "little")
 
 
-def convertFromNumber(n):
+def convert_from_number(number):
     """Convert from a numerical value to a textual representation."""
-    return n.to_bytes(math.ceil(n.bit_length() / 8), LITTLE).decode()
+    return number.to_bytes(math.ceil(number.bit_length() / 8), LITTLE).decode()
 
 
 def createfiasco(function):
@@ -27,8 +27,8 @@ def createfiasco(function):
     def wrap(*args, **kwargs):
         function_name = function.__name__
         function_module = inspect.getmodule(function)
-        function_id = convertToNumber(function_name + SINGLE_BLANK_SPACE + str(function_module))
-        function_details_from_id = convertFromNumber(function_id)
+        function_id = convert_to_number(function_name + SINGLE_BLANK_SPACE + str(function_module))
+        function_details_from_id = convert_from_number(function_id)
         print(
             f"function {function_name} found in module {function_module} has id {function_id}"
         )
