efc33b61d37ec31a7da52f77c7589c5a4931dc3e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 19:05:08 -0400

    feat: Start to build the JSON file of functions for tracking in fiasco.

--- dataanalysis/dataanalysis/fiasco.py
+++ dataanalysis/dataanalysis/fiasco.py
@@ -2,12 +2,14 @@
 
 from functools import wraps
 
+import json
 import inspect
-
 import math
 
 LITTLE = "little"
 SINGLE_BLANK_SPACE = " "
+
+fiasco_tracker = {}
 
 
 def convert_to_number(textual):
@@ -20,15 +22,30 @@ def convert_from_number(number):
     return number.to_bytes(math.ceil(number.bit_length() / 8), LITTLE).decode()
 
 
+def save_tracker():
+    """Save the function fiasco tracker."""
+    print("Saving the tracker")
+    with open("fiasco.json", "w") as fiasco_json_file:
+        json.dump(fiasco_tracker, fiasco_json_file)
+
+
 def createfiasco(function):
     """Define a fiasco function that can observe and control function execution."""
 
     @wraps(function)
     def wrap(*args, **kwargs):
+        # extract the name of the function
         function_name = function.__name__
+        # extract the module of the function
         function_module = inspect.getmodule(function)
+        # convert the name and module of the function into an integer identifier
+        # note that it may not be guaranteed that this number is unique
         function_id = convert_to_number(function_name + SINGLE_BLANK_SPACE + str(function_module))
         function_details_from_id = convert_from_number(function_id)
+        # create a key value pair of (function_id, False) to indicate that this
+        # specific function has not yet been fiasco-ified (i.e., turned off) during
+        # the execution of the test suite
+        fiasco_tracker[function_id] = False
         print(
             f"function {function_name} found in module {function_module} has id {function_id}"
         )
