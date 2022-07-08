68f682b02e07e04da0202c3a4217c5014f157d78
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 20:21:23 -0400

    feat: Add the function_setup_teardown test fixture in conftest.

--- dataanalysis/tests/conftest.py
+++ dataanalysis/tests/conftest.py
@@ -14,3 +14,12 @@ def session_setup_teardown():
     fiasco.save_tracker()
     bedlam.save_tracker()
     print("After test suite execution")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def function_setup_teardown(request):
+    """Execute arbitrary operations before and after running a test suite."""
+    test_name = request.function.__name__
+    print(f"Before test case execution: {test_name}")
+    yield
+    print("After test case execution")
