dbe1da189ca7eb65c77b0e4ae4daba0e50178aed
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-07-07 20:41:10 -0400

    fix: Use better test case names in the test_summarize.

--- dataanalysis/tests/test_summarize.py
+++ dataanalysis/tests/test_summarize.py
@@ -12,21 +12,21 @@ def test_summarize_empty_number_list_mean():
     assert math.isnan(mean)
 
 
-def test_summarize_pos_neg_number_list():
+def test_summarize_pos_neg_number_list_mean():
     """Ensure that a "cancel out" list of numbers summarizes correctly."""
     data_list_numbers = [-10.0, 10.0]
     mean = summarize.compute_mean(data_list_numbers)
     assert mean == 0.0
 
 
-def test_summarize_equal_number_list():
+def test_summarize_equal_number_list_mean():
     """Ensure that an equal number list of numbers summarizes correctly."""
     data_list_numbers = [10.0, 10.0]
     mean = summarize.compute_mean(data_list_numbers)
     assert mean == 10.0
 
 
-def test_summarize_different_number_list():
+def test_summarize_different_number_list_mean():
     """Ensure that an equal number list of numbers summarizes correctly."""
     data_list_numbers = [5.0, 10.0]
     mean = summarize.compute_mean(data_list_numbers)
